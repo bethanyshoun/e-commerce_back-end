0242837f6745e950d6dd5c98830ca6493f477b32
bethanyshoun <bethanyshoun@gmail.com>
2022-10-01 22:30:26 -0400

    index.js file

--- models/index.js
+++ models/index.js
@@ -5,12 +5,28 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+  onDelete: 'SET NULL'
+});
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: 'product_id'
+});
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  as: 'product_tag',
+  foreignKey: 'product_id',
+  onDelete: 'SET NULL'
+});
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  as: 'product_tag',
+  foreignKey: 'tag',
+  onDelete: 'SET NULL'
+});
 
 module.exports = {
   Product,
@@ -18,3 +34,5 @@ module.exports = {
   Tag,
   ProductTag,
 };
+
+
