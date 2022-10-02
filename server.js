9b5aa043990491c6a9185de94cfd7364e6d9bda0
bethanyshoun <bethanyshoun@gmail.com>
2022-10-01 21:53:54 -0400

    intial imports, env/gitignore updates

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const routes = require('./routes');
+// import sequelize connection
+import { Sequelize } from "sequelize"
+
+const connection = new Sequelize(
+    process.env.DATABASE_NAME,
+    process.env.DATABASE_USER,
+    process.env.DATABASE_PASSWORD,
+    {
+        host: process.env.DATABASE_URL,
+        port: process.env.DATABASE_PORT,
+        dialect: "mysql",
+        logging: false,
+        define: {
+            // prevent sequelize from pluralizing table names
+            freezeTableName: true,
+        },
+    }
+)
+
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(routes);
+
+// sync sequelize models to the database, then turn on the server
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}!`);
+});
+
+export { connection as default }
